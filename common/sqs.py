695381872eb4ca3eddcb44f03a5224f8b8aff4d3
Bernardo Couto <bernardo.couto@cortex-intelligence.com>
2022-01-03 23:41:35 -0300

    corrigindo o nome dos buckets

--- common/sqs.py
+++ common/sqs.py
@@ -5,5 +5,13 @@ class SQS:
     def __init__(self):
         self.client = boto3.client('sqs')
 
-    def send_message(self):
-        pass
+    def send_message(self, message_body, queue_url):
+        try:
+            self.client.send_message(
+                MessageBody=message_body,
+                QueueUrl=queue_url
+            )
+        except self.client.exceptions.InvalidMessageContents as exception:
+            print(exception)
+        except self.client.exceptions.UnsupportedOperation as exception:
+            print(exception)
