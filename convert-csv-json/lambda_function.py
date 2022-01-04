57883ec5c1bb24fa82797553fc074c408352fc33
Bernardo Couto <bernardo.couto@cortex-intelligence.com>
2022-01-04 16:38:09 -0300

    investigando erro da conversao

--- convert-csv-json/lambda_function.py
+++ convert-csv-json/lambda_function.py
@@ -17,12 +17,18 @@ def lambda_handler(event, context):
     queue_name = os.environ.get('AWS_SQS_RAW_JSON_TO_FIREHOSE', 'raw-json-to-firehose')
     queue_url = sqs.get_queue_url(queue_name=queue_name)
 
+    columns_names = [
+        'id',
+        'name'
+    ]
+
     if 'Records' in event:
         for record in event['Records']:
             body = json.loads(record['body'])
             receipt_handle = record['receiptHandle']
             logger.info(f'Reading the CSV on AWS S3: s3://{body["bucket"]}/{body["key"]}')
             dfs = wr.s3.read_csv(
+                names=columns_names,
                 path=[f's3://{body["bucket"]}/{body["key"]}'],
                 sep=';'
             )
