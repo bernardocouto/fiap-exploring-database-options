baaa988ff3e464c8999c16236a852187b3bfa945
Bernardo Couto <bernardo.couto@cortex-intelligence.com>
2022-01-04 18:37:37 -0300

    testando criacao do kinesis firehose

---
+++ common/firehose.py
@@ -0,0 +1,24 @@
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Firehose:
+
+    def __init__(self):
+        self.client = boto3.client('firehose')
+
+    def put_record(self, record):
+        try:
+            self.client.put_record(
+                DeliveryStreamName='',
+                Record={'Data': record}
+            )
+        except self.client.exceptions.InvalidArgumentException as exception:
+            logger.error(f'Invalid argument: {exception}')
+        except self.client.exceptions.InvalidKMSResourceException as exception:
+            logger.error(f'Invalid KMS resource: {exception}')
+        except self.client.exceptions.ResourceNotFoundException as exception:
+            logger.error(f'Resource not found: {exception}')
+        except self.client.exceptions.ServiceUnavailableException as exception:
+            logger.error(f'Service unavailable: {exception}')
