8b1bbcf797cc3e14e604c1e533855ffc73a01bd1
Bernardo Couto <bernardo.couto@cortex-intelligence.com>
2022-01-04 00:30:18 -0300

    testando envio das mensagens para o sqs

--- read-from-raw-to-sqs/lambda_function.py
+++ read-from-raw-to-sqs/lambda_function.py
@@ -1,14 +1,23 @@
 from common.sqs import SQS
 
+import json
 import os
 
 def lambda_handler(event, context):
 
-    # TODO: Sento to SQS
-    print(event)
-
-    queue_name = os.environ.get('AWS_SQS_RAW_JSON_TO_FIREHOSE', 'row-json-to-firehose')
+    queue_name = os.environ.get('AWS_SQS_RAW_JSON_TO_FIREHOSE', 'raw-json-to-firehose')
 
     sqs = SQS()
+
     queue_url = sqs.get_queue_url(queue_name=queue_name)
-    sqs.send_message(message_body=None, queue_url=queue_url)
+
+    if 'Records' in event:
+        for record in event['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+            message_body = json.dumps({
+                'bucket': bucket,
+                'key': key
+            })
+            sqs.send_message(message_body=message_body, queue_url=queue_url)
+
--- read-from-small-to-sqs/lambda_function.py
+++ read-from-small-to-sqs/lambda_function.py
@@ -1,4 +1,22 @@
+from common.sqs import SQS
+
+import json
+import os
+
 def lambda_handler(event, context):
 
-    # TODO: Send to SQS
-    print(event)
+    queue_name = os.environ.get('AWS_SQS_SMALL_FILES_CSV', 'small-files-csv')
+
+    sqs = SQS()
+
+    queue_url = sqs.get_queue_url(queue_name=queue_name)
+
+    if 'Records' in event:
+        for record in event['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+            message_body = json.dumps({
+                'bucket': bucket,
+                'key': key
+            })
+            sqs.send_message(message_body=message_body, queue_url=queue_url)
