80beedd2d2cc8e816e2008725fa9f3799a166476
Bernardo Couto <bernardo.couto@cortex-intelligence.com>
2022-01-03 23:54:10 -0300

    testando a trigger da lambda com o s3

--- common/sqs.py
+++ common/sqs.py
@@ -1,9 +1,21 @@
 import boto3
+import logging
+
+logger = logging.getLogger(__name__)
 
 class SQS:
 
     def __init__(self):
         self.client = boto3.client('sqs')
+
+    def get_queue_url(self, queue_name):
+        try:
+            queue_url = self.client.get_queue_url(
+                QueueName=queue_name
+            )
+            return queue_url.get('QueueUrl')
+        except self.client.exceptions.QueueDoesNotExist as exception:
+            logger.error(f'Queue does not exist: {exception}')
 
     def send_message(self, message_body, queue_url):
         try:
@@ -12,6 +24,6 @@ class SQS:
                 QueueUrl=queue_url
             )
         except self.client.exceptions.InvalidMessageContents as exception:
-            print(exception)
+            logger.error(f'Invalid message contents: {exception}')
         except self.client.exceptions.UnsupportedOperation as exception:
-            print(exception)
+            logger.error(f'Unsupported operation: {exception}')
--- read-from-raw-to-sqs/lambda_function.py
+++ read-from-raw-to-sqs/lambda_function.py
@@ -1,4 +1,14 @@
+from common.sqs import SQS
+
+import os
+
 def lambda_handler(event, context):
 
     # TODO: Sento to SQS
     print(event)
+
+    queue_name = os.environ.get('AWS_SQS_RAW_JSON_TO_FIREHOSE', 'row-json-to-firehose')
+
+    sqs = SQS()
+    queue_url = sqs.get_queue_url(queue_name=queue_name)
+    sqs.send_message(message_body=None, queue_url=queue_url)
