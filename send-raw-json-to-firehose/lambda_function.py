12492d1ead37c38300f7dc2e0efdbedf86cbceb0
Bernardo Couto <bernardocouto@icloud.com>
2022-01-06 00:40:24 -0300

    adicionando logs

--- send-raw-json-to-firehose/lambda_function.py
+++ send-raw-json-to-firehose/lambda_function.py
@@ -29,12 +29,12 @@ def lambda_handler(event, context):
                 logger.info(f'Send record to AWS Kinesis Firehose JSON: {row[1].to_json()}')
                 firehose.put_record(
                     delivery_stream_name=delivery_stream_name_json,
-                    record=json.loads(row[1].to_json())
+                    record=json.dumps(row[1].to_json(), indent=2).encode('utf-8')
                 )
                 logger.info(f'Send record to AWS Kinesis Firehose Parquet: {row[1].to_json()}')
                 firehose.put_record(
                     delivery_stream_name=delivery_stream_name_parquet,
-                    record=json.loads(row[1].to_json())
+                    record=json.dumps(row[1].to_json(), indent=2).encode('utf-8')
                 )
 
     logger.info('End of AWS Lambda run')
